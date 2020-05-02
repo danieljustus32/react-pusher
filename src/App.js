600f605f1807c55d3216a66b3e23752d4445224f
danieljustus32 <daniel.justus32@gmail.com>
2020-05-01 22:41:27 -0500

    Basic auth working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React from "react";
+import NavBar from "./components/NavBar";
 
-const MeetupContext = React.createContext();
+// New - import the React Router components, and the Profile page component
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
 
-const initialState = {
-  title: 'React Online Meetup',
-  date: Date()
-};
-
-const App = () => (
-  <MeetupContext.Provider value={initialState}>
-    <MeetupContext.Consumer>
-      {props => (
-        <div>
-          <h1>{props.title}</h1>
-          <span>{props.date}</span>
-        </div>
-      )}
-    </MeetupContext.Consumer>
-  </MeetupContext.Provider>
-);
+function App() {
+  return (
+    <div className="App">
+      {/* Don't forget to include the history module */}
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
 
 export default App;
