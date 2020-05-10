ff6e81a8b6617a3b2296792b67fcbd80234588a7
danieljustus32 <daniel.justus32@gmail.com>
2020-05-10 16:23:20 -0500

    Import Pusher

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
+import Pusher from "pusher-js";
 
 // Main Components
 import NavBar from "./components/NavBar";
